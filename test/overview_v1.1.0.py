9676247b628f6598a5cca15f94c7d9c215b7d132
Jahongir Sobirov <69193276+Jahongir2007@users.noreply.github.com>
2021-04-05 10:55:49 +0500

    Create overview_v1.1.0.py

---
+++ test/overview_v1.1.0.py
@@ -0,0 +1,9 @@
+import  pymetry
+pym = pymetry
+pym.adjaccor("unknown", 135)
+pym.valtri("unknown", 20, 60)
+pym.perm(12, 24)
+pym.sqside("unknown", 24, 49)
+pym.pert(21, 13, 15)
+pym.trside("unknown", 12, 12, 36)
+pym.aboutme()
